6bbd2ef02f2d42ab73babb2c9ba0a8c56596c232
pamilerin17 <113692021+pamilerin17@users.noreply.github.com>
2024-03-27 09:10:58 +0100

    Add files via upload

---
+++ acme/build/js/main.js
@@ -0,0 +1,13 @@
+var bar = document.getElementById("bar");
+var header = document.getElementById("header");
+let navBar = document.querySelector('.navbar');
+
+bar.addEventListener('click', ()=>{
+    navBar.classList.toggle('active');
+    bar.classList.toggle('bx-x');
+})
+
+
+// const button=()=>{
+//     const
+// }
